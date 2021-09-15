796927c4ada9ccaac5b522384db0993fd81d5069
hannah <hannah@taling.me>
2021-09-15 15:44:48 +0900

    프로파일별 환경변수 설정

--- src/test.js
+++ src/test.js
@@ -1,20 +1,26 @@
 'use strict';
 
-const axios = require('axios');
+// const axios = require('axios');
 const response = (statusCode, body) => ({
     statusCode: statusCode,
     body: JSON.stringify(body),
 });
 
+const env = {
+    TALING_ENDPOINT: process.env.TALING_ENDPOINT,
+    TALING_ADMIN_ENDPOINT: process.env.TALING_ADMIN_ENDPOINT,
+}
+
 module.exports.testCall = async (event) => {
     try {
-        await axios.get(process.env.TALING_ENDPOINT + '/api3/talent/liveroom.php');
+        //await axios.get(process.env.TALING_ENDPOINT + '/api3/talent/liveroom.php');
         return response(200, {
             message: 'OK testCall',
+            env: env,
             input: event,
         });
     } catch (error) {
-        return response(error.response.status, {
+        return response(error.response?.status, {
             message: error.message,
             input: event,
         });
@@ -23,9 +29,10 @@ module.exports.testCall = async (event) => {
 
 module.exports.testCron = async (event) => {
     try {
-        await axios.get(process.env.TALING_ENDPOINT + '/api3/talent/liveroom.php');
+        //await axios.get(process.env.TALING_ENDPOINT + '/api3/talent/liveroom.php');
         return response(200, {
             message: 'OK testCron',
+            env: env,
             input: event,
         });
     } catch (error) {
